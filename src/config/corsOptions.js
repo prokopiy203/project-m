f4cff77931dd240576342cf9f59aea8ae8247a03
illia_dp <illiachernysh.dp@gmail.com>
2025-10-20 22:12:14 +0200

    add my personal swagger

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   'https://plantains-app-dev.vercel.app', // dev
   'https://plantains-app-client.vercel.app', //product
   'https://project-m-7jdz.onrender.com', //backend swagger
+  'https://project-m-m305.onrender.com/',
 ];
 
 export const corsOptions = {
